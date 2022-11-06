d90a6f82da2835286c52e0b15ee8748e048d6d99
Anthonia Nwanya <105661899+AnthoniaNwanya@users.noreply.github.com>
2022-11-06 11:20:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use("/auth", JwtRoute);
 app.use("/blog", BlogRoute);
 app.use("/user", UserRoute);
 
+app.all("/", (req, res) => {
+  res.redirect('https://github.com/AnthoniaNwanya/Blogged_Up/blob/9ad5d75ee16884e75ba13ad121474c40a2970205/README.md')
+} )
 
 app.use(function (err, req, res, next) {
   res.status(500);
