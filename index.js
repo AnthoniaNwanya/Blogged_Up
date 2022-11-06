024e766904ba4c2dfd405fdd5640835928304d3f
Anthonia Nwanya <105661899+AnthoniaNwanya@users.noreply.github.com>
2022-11-06 02:37:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", "views");
+
 
 app.use("/auth", JwtRoute);
 app.use("/blog", BlogRoute);
 app.use("/user", UserRoute);
 
-app.get('/', (req, res) => {
-  res.render("index")
-})
 
 app.use(function (err, req, res, next) {
   res.status(500);
